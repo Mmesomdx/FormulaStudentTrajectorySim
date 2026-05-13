49ae42801978702c1915db1bb59cdf50e27fa45c
Mmeso <94660563+Mmesomdx@users.noreply.github.com>
2026-05-13 16:53:09 +0100

    Update sim.py

--- sim.py
+++ sim.py
@@ -1,7 +1,8 @@
 import turtle
+import random
 
 sc = turtle.Screen()
-sc.setup(width=500, height=500)
+sc.setup(width=700, height=700)
 sc.bgcolor("green")
 sc.tracer(0)
 sprites = ["orange_cone.gif", "blue_cone.gif"]
@@ -30,7 +31,21 @@ def midpoint(p1, p2):
     my = (p1[1] + p2[1]) / 2
     return (mx, my)
     
+#laplacian smoothing.
+def flatten(points):
+    new_points = []
+    # Add the first point as is
+    new_points.append(points[0])
     
+    # Average every middle point with the one before it and after it
+    for i in range(1, len(points) - 1):
+        x = (points[i-1][0] + points[i][0] + points[i+1][0]) / 3
+        y = (points[i-1][1] + points[i][1] + points[i+1][1]) / 3
+        new_points.append((x, y))
+        
+    # Add the last point as is
+    new_points.append(points[-1])
+    return new_points
     
     
     
@@ -63,19 +78,25 @@ class Car(turtle.Turtle):
 
 orange_cones = []
 blue_cones = []
-track_width = 100
-cone_spacing = 50
+track_width = 150
+cone_spacing = 25
+
+horizontal_noise = 0
+vertical_noise = 0
 
 
 
 
-for i in range(-4,4,1):
+for i in range(-10,10,1):
     y = i * cone_spacing
     offset = 0.005 * (y**2)  # tweak 0.02 to make the arc more or less curved
     
     
-    orange_cones.append(Cone("orange", pos=(track_width + offset - 50,i*cone_spacing)))
-    blue_cones.append(Cone("blue", pos=(-track_width + offset - 50, i*cone_spacing)))
+    orange_cones.append(Cone("orange", pos=(track_width + offset - 100 + random.uniform(-horizontal_noise,horizontal_noise),i*cone_spacing + random.uniform(-vertical_noise,vertical_noise))))
+    blue_cones.append(Cone("blue", pos=(-track_width + offset - 100 + random.uniform(-horizontal_noise,horizontal_noise), i*cone_spacing + random.uniform(-vertical_noise,vertical_noise))))
+    
+
+blue_cones[3].setx(-1000)
     
 #print orange cones
 print("ORANGE CONE COORDS:")
@@ -108,11 +129,40 @@ for i in range(len(blue_cones)-1):
 mid_points = []
 for i in range(len(orange_cones)):
     mid_points.append(midpoint(orange_cones[i].getPos(), blue_cones[i].getPos()))
+    
 
 for i in range(len(mid_points)-1):
     mid_point_current = mid_points[i]
     mid_point_next = mid_points[i+1]
     draw_line("yellow", mid_point_current, mid_point_next)
+    
+
+mid_points = flatten(mid_points)
+
+for i in range(len(mid_points)-1):
+    mid_point_current = mid_points[i]
+    mid_point_next = mid_points[i+1]
+    draw_line("magenta", mid_point_current, mid_point_next)
+
+
+
+mid_points = flatten(mid_points)
+
+for i in range(len(mid_points)-1):
+    mid_point_current = mid_points[i]
+    mid_point_next = mid_points[i+1]
+    draw_line("cyan", mid_point_current, mid_point_next)
+    
+
+#Ultra smoothed line!
+
+for i in range(10):
+    mid_points = flatten(mid_points)
+
+for i in range(len(mid_points)-1):
+    mid_point_current = mid_points[i]
+    mid_point_next = mid_points[i+1]
+    draw_line("white", mid_point_current, mid_point_next)
 
 
 
